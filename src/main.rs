cec11f86c9b98b688e2ceb55f8f6c2e45f71ad42
Christophe Sadoine <chris@indefini.org>
2016-08-09 19:21:35 +0900

    more func

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,12 @@ use std::ffi::CString;
 
 fn main() {
     let display = unsafe { wayland::wl_display_create() };
+
     println!("created a display : {:?}", display);
+
+    //let client = unsafe { wayland::wl_client_create() };
+
+    unsafe { wayland::wl_display_destroy(display); }
 }
 
 
